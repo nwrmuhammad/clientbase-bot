1592286e4b0d269fa506d41628af7420ae5d46b8
nwrmuhammad <mayorbi07@gmail.com>
2025-08-09 12:00:27 +0500

    middlewear updated

--- src/middleware/access_admin.js
+++ src/middleware/access_admin.js
@@ -1,19 +1,32 @@
+const admins = ["8025873679", "1118457274", "7220439451"];
+
 function accessMiddleware(ctx, next) {
-  if (
-    (ctx.chat && ctx.chat.id == process.env.ADMIN_nvr) ||
-    process.env.ADMIN_shrz
-  ) {
-    return next();
+  if (!admins.includes(String(ctx.chat.id))) {
+    ctx.reply("❌ Siz botdan foydalanish huquqiga ega emassiz.", {
+      reply_markup: { remove_keyboard: true },
+    });
   } else {
-    ctx.reply(
-      console.log(ctx.chat.id),
-      "❌ Siz botdan foydalanish huquqiga ega emassiz.",
-      {
-        reply_markup: { remove_keyboard: true },
-      }
-    );
-    return;
+    return next();
   }
 }
 
 module.exports = accessMiddleware;
+
+//   if (
+//     (ctx.chat && ctx.chat.id == process.env.ADMIN_nvr) ||
+//     process.env.ADMIN_shrz
+//   ) {
+//     return next();
+//   } else {
+//     ctx.reply(
+//       console.log(ctx.chat.id),
+//       "❌ Siz botdan foydalanish huquqiga ega emassiz.",
+//       {
+//         reply_markup: { remove_keyboard: true },
+//       }
+//     );
+//     return;
+//   }
+
+//   console.log(!admins.includes(++ctx.chat.id));
+//   console.log(ctx.chat.id);
