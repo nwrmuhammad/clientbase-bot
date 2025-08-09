11fde41b5fcc9274b96cb6b81114ee55e9606f07
nwrmuhammad <mayorbi07@gmail.com>
2025-08-09 11:29:28 +0500

    chanded env

--- src/index.js
+++ src/index.js
@@ -1,109 +1,96 @@
-require('dotenv').config();
-const { Telegraf, Markup } = require('telegraf');
-const fs = require('fs');
-const accessMiddleware = require('./middleware/access_admin');
+require("dotenv").config();
+const { Telegraf, Markup } = require("telegraf");
+const fs = require("fs");
+const accessMiddleware = require("./middleware/access_admin");
 
 const bot = new Telegraf(process.env.TOKEN);
 
-let sessions = {}; 
-
+let sessions = {};
 
 function loadCustomers() {
-    if (fs.existsSync('customers.json')) {
-        return JSON.parse(fs.readFileSync('customers.json', 'utf8'));
-    }
-    return [];
+  if (fs.existsSync("customers.json")) {
+    return JSON.parse(fs.readFileSync("customers.json", "utf8"));
+  }
+  return [];
 }
-
-
 
 function saveCustomer(customer) {
-    let customers = loadCustomers();
-    customers.push(customer);
-    fs.writeFileSync('customers.json', JSON.stringify(customers, null, 2));
+  let customers = loadCustomers();
+  customers.push(customer);
+  fs.writeFileSync("customers.json", JSON.stringify(customers, null, 2));
 }
 
-bot.use(accessMiddleware)
+bot.use(accessMiddleware);
 
 bot.start((ctx) => {
-    console.log(ctx.from.id);
-    ctx.reply(
-        "Mijozlar ro`yxatini olish va ularga xabar yuborish , faqat so`kilmasin 👊🏻",
-        Markup.keyboard([
-            ["➕ Mijoz qo`shish", "📋 Mijozlar ro`yxati"]
-        ]).resize()
-    );
+  console.log(ctx.from.id);
+  ctx.reply(
+    "Mijozlar ro`yxatini olish va ularga xabar yuborish , faqat so`kilmasin 👊🏻",
+    Markup.keyboard([["➕ Mijoz qo`shish", "📋 Mijozlar ro`yxati"]]).resize()
+  );
 });
 
 // Tugmalar ishlashi
 bot.hears("➕ Mijoz qo`shish", (ctx) => {
-    let id = ctx.chat.id;
-    sessions[id] = { step: "get_name" };
-    ctx.reply("Mijoz ismini kiriting:");
+  let id = ctx.chat.id;
+  sessions[id] = { step: "get_name" };
+  ctx.reply("Mijoz ismini kiriting:");
 });
 
-
 bot.hears("📋 Mijozlar ro`yxati", (ctx) => {
-    let customers = loadCustomers();
+  let customers = loadCustomers();
 
-    if (!customers || customers.length === 0) {
-        return ctx.reply("📭 Hozircha mijozlar yo‘q.");
-    }
+  if (!customers || customers.length === 0) {
+    return ctx.reply("📭 Hozircha mijozlar yo‘q.");
+  }
 
-    let text = "📋 Mijozlar ro‘yxati:\n\n";
-    customers.forEach((c, i) => {
-        text += `${i + 1}) \n <b>Ismi:</b> <i>${c.name}</i>, \n <b>Raqami:</b> <i>${c.phone || "📞 yo‘q"}</i>, \n <b>Nima sotib olgan:</b> <i>${c.purchased_item || "📦 yo‘q"}</i> \n`;
-    });
+  let text = "📋 Mijozlar ro‘yxati:\n\n";
+  customers.forEach((c, i) => {
+    text += `${i + 1}) \n <b>Ismi:</b> <i>${c.name}</i>, \n <b>Raqami:</b> <i>${
+      c.phone || "📞 yo‘q"
+    }</i>, \n <b>Nima sotib olgan:</b> <i>${
+      c.purchased_item || "📦 yo‘q"
+    }</i> \n`;
+  });
 
-    ctx.reply(text , { parse_mode: "HTML" });
+  ctx.reply(text, { parse_mode: "HTML" });
 });
 
 bot.on("text", (ctx) => {
-    let id = ctx.chat.id;
-    let session = sessions[id];
+  let id = ctx.chat.id;
+  let session = sessions[id];
 
-    if (!session) return "/start bosing"
+  if (!session) return "/start bosing";
 
-    if (session.step === "get_name") {
-        session.name = ctx.message.text;
-        session.step = "get_item";
-        ctx.reply("Xarid qilingan mahsulotni kiriting:");
-    } 
-    else if (session.step === "get_item") {
-        session.purchased_item = ctx.message.text;
-        session.step = "get_date";
-        ctx.reply("Qachon xarid qilingan: Misol uchun 2023-29-01");
-       
-    } 
-    else if (session.step === "get_date") {
-        session.date = ctx.message.text;
-        session.step = "get_contact";
-        ctx.reply("Mijozning telefon raqamini yuboring")
-       
-    }
-    else if (session.step === "get_contact") {
-        session.contact = ctx.message.text;
+  if (session.step === "get_name") {
+    session.name = ctx.message.text;
+    session.step = "get_item";
+    ctx.reply("Xarid qilingan mahsulotni kiriting:");
+  } else if (session.step === "get_item") {
+    session.purchased_item = ctx.message.text;
+    session.step = "get_date";
+    ctx.reply("Qachon xarid qilingan: Misol uchun 2023-29-01");
+  } else if (session.step === "get_date") {
+    session.date = ctx.message.text;
+    session.step = "get_contact";
+    ctx.reply("Mijozning telefon raqamini yuboring");
+  } else if (session.step === "get_contact") {
+    session.contact = ctx.message.text;
 
-         let customers = {
-            name: session.name,
-            phone: session.contact,
-            date: session.date,
-            purchased_item: session.purchased_item
-        }
-        saveCustomer(customers)
+    let customers = {
+      name: session.name,
+      phone: session.contact,
+      date: session.date,
+      purchased_item: session.purchased_item,
+    };
+    saveCustomer(customers);
 
-        ctx.reply(
-            `✅ Rahmat, ${session.name} okamzani! Ma'lumotlari saqlandi.`,
-            Markup.keyboard([
-                ["➕ Add Customer", "📋 List of Customers"]
-            ]).resize()
-        );
-        delete sessions[id];
-    }
-
+    ctx.reply(
+      `✅ Rahmat, ${session.name} okamzani! Ma'lumotlari saqlandi.`,
+      Markup.keyboard([["➕ Mijoz qo`shish", "📋 Mijozlar ro`yxati"]]).resize()
+    );
+    delete sessions[id];
+  }
 });
-
-
-
 
 bot.launch();
--- src/middleware/access_admin.js
+++ src/middleware/access_admin.js
@@ -1,16 +1,19 @@
-
 function accessMiddleware(ctx, next) {
-    if (ctx.chat && ctx.chat.id == process.env.ADMIN_nvr) {
-        return next(); 
-    } else {
-         ctx.reply(
-            '❌ Siz botdan foydalanish huquqiga ega emassiz.',
-            {
-                reply_markup: { remove_keyboard: true }
-            }
-        );
-        return;
-    }
+  if (
+    (ctx.chat && ctx.chat.id == process.env.ADMIN_nvr) ||
+    process.env.ADMIN_shrz
+  ) {
+    return next();
+  } else {
+    ctx.reply(
+      console.log(ctx.chat.id),
+      "❌ Siz botdan foydalanish huquqiga ega emassiz.",
+      {
+        reply_markup: { remove_keyboard: true },
+      }
+    );
+    return;
+  }
 }
 
 module.exports = accessMiddleware;
